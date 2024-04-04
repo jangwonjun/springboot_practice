8027ffcee358dd7da7d15b450e5034835f05ddff
jangwonjun <won2005won@naver.com>
2024-04-04 14:30:06 +0900

    test

---
+++ src/main/java/me/wonjunjang/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,13 @@
+package me.wonjunjang.springbootdeveloper;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class SpringBootDeveloperApplication {
+
+    public static void main(String[] args) {
+        run(SpringBootDeveloperApplication.class, args);
+    }
+}
